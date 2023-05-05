ce805c2046bcebb4495360d9ef6e670d451579c9
Gabriel R <gabriel1706.com@gmail.com>
2023-05-05 19:50:45 -0300

    all files

---
+++ rodents_r_us/.next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/rodent":["static\u002Fchunks\u002Fpages\u002Frodent.js"],sortedPages:["\u002F_app","\u002F_error","\u002Frodent"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
